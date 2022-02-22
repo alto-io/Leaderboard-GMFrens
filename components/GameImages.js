d9a1d92fd282ebb1f5de04688fb487d284f729ac
Michele Picozzi <michele.picozzi@hotmail.it>
2022-02-22 11:47:44 +0000

    seven games and links

--- components/GameImages.js
+++ components/GameImages.js
@@ -23,11 +23,29 @@ export default function GameImages(props) {
             gameName: "LFG Racing",
             gameLink: "https://beyondrealitybox.itch.io/kart-frenzy"
         },
+        {
+            gameImage: "/assets/arcadeumArena.png",
+            gameName: "Arcadeum Arena",
+            gameLink: "https://test.outplay.games/games/c158dc81-9212-4472-acf5-d10ae5177c09/"
+        },
+        {
+            gameImage: "/assets/robotWars.png",
+            gameName: "Robot Wars",
+            gameLink: "https://test.outplay.games/games/726c662c-8550-4233-894c-65e88ccfd700/"
+        },
+        {
+            gameImage: "/assets/triska.png",
+            gameName: "Triska",
+            gameLink: "https://test.outplay.games/games/aeec95c2-ec3e-494f-85f6-9a6237ee2d0b/"
+        },
+        
+        
+        
 
     ]
 
     return (
-        <div className='relative mx-auto max-w-[1430px] h-[400px] flex bg-red-200 overflow-scroll scrollbar-hide'>
+        <div className='relative mx-auto max-w-[1430px] h-[400px] flex overflow-scroll scrollbar-hide'>
             <div className='absolute flex animate-slide'>
                 {/* DISPLAY EXACTLY 7 ELEMENTS */}
                 {gameArray.map((game, index) => (
@@ -47,47 +65,8 @@ export default function GameImages(props) {
                         </div>
                     </div>
                 ))}
-                 <div className='bg-gray-100 h-96 rounded-xl overflow-hidden m-[15px]'>
-                    <img
-                        className='bg-black w-64 h-64' 
-                        src=""
-                        alt="gameImage"
-                    />
-                    <div className='flex flex-col h-28 text-black p-2 justify-between'>
-                        <p className='w-full font-bold'>Game Name</p>
-                        <button className='flex flex-start border w-20 py-1 px-1 text-center rounded-xl'>
-                            <p className='flex mx-auto text-xs font-bold'>Play Now</p>    
-                        </button>
-                    </div>
-                </div>
-                <div className='bg-gray-100 h-96 rounded-xl overflow-hidden m-[15px]'>
-                    <img
-                        className='bg-black w-64 h-64' 
-                        src=""
-                        alt="gameImage"
-                    />
-                    <div className='flex flex-col h-28 text-black p-2 justify-between'>
-                        <p className='w-full font-bold'>Game Name</p>
-                        <button className='flex flex-start border w-20 py-1 px-1 text-center rounded-xl'>
-                            <p className='flex mx-auto text-xs font-bold'>Play Now</p>    
-                        </button>
-                    </div>
-                </div>
-                <div className='bg-gray-100 h-96 rounded-xl overflow-hidden m-[15px]'>
-                    <img
-                        className='bg-black w-64 h-64' 
-                        src=""
-                        alt="gameImage"
-                    />
-                    <div className='flex flex-col h-28 text-black p-2 justify-between'>
-                        <p className='w-full font-bold'>Game Name</p>
-                        <button className='flex flex-start border w-20 py-1 px-1 text-center rounded-xl'>
-                            <p className='flex mx-auto text-xs font-bold'>Play Now</p>    
-                        </button>
-                    </div>
-                </div>
                 {/* REPEAT first 5 ELEMENTS */}
-                {gameArray.map((game, index) => (
+                {gameArray.slice(0, 5).map((game, index) => (
                     <div className='bg-gray-100 h-96 w-64 rounded-xl overflow-hidden m-[15px]' key={index}>
                         <img
                             className='bg-black w-64 h-64' 
@@ -104,58 +83,6 @@ export default function GameImages(props) {
                         </div>
                     </div>
                 ))}
-                <div className='bg-gray-100 h-96 rounded-xl overflow-hidden m-[15px]'>
-                    <img
-                        className='bg-black w-64 h-64' 
-                        src=""
-                        alt="gameImage"
-                    />
-                    <div className='flex flex-col h-28 text-black p-2 justify-between'>
-                        <p className='w-full font-bold'>Game Name</p>
-                        <button className='flex flex-start border w-20 py-1 px-1 text-center rounded-xl'>
-                            <p className='flex mx-auto text-xs font-bold'>Play Now</p>    
-                        </button>
-                    </div>
-                </div>
-                <div className='bg-gray-100 h-96 rounded-xl overflow-hidden m-[15px]'>
-                    <img
-                        className='bg-black w-64 h-64' 
-                        src=""
-                        alt="gameImage"
-                    />
-                    <div className='flex flex-col h-28 text-black p-2 justify-between'>
-                        <p className='w-full font-bold'>Game Name</p>
-                        <button className='flex flex-start border w-20 py-1 px-1 text-center rounded-xl'>
-                            <p className='flex mx-auto text-xs font-bold'>Play Now</p>    
-                        </button>
-                    </div>
-                </div>
-                <div className='bg-gray-100 h-96 rounded-xl overflow-hidden m-[15px]'>
-                    <img
-                        className='bg-black w-64 h-64' 
-                        src=""
-                        alt="gameImage"
-                    />
-                    <div className='flex flex-col h-28 text-black p-2 justify-between'>
-                        <p className='w-full font-bold'>Game Name</p>
-                        <button className='flex flex-start border w-20 py-1 px-1 text-center rounded-xl'>
-                            <p className='flex mx-auto text-xs font-bold'>Play Now</p>    
-                        </button>
-                    </div>
-                </div>
-                <div className='bg-gray-100 h-96 rounded-xl overflow-hidden m-[15px]'>
-                    <img
-                        className='bg-black w-64 h-64' 
-                        src=""
-                        alt="gameImage"
-                    />
-                    <div className='flex flex-col h-28 text-black p-2 justify-between'>
-                        <p className='w-full font-bold'>Game Name</p>
-                        <button className='flex flex-start border w-20 py-1 px-1 text-center rounded-xl'>
-                            <p className='flex mx-auto text-xs font-bold'>Play Now</p>    
-                        </button>
-                    </div>
-                </div>
             </div>
         </div>
     )
